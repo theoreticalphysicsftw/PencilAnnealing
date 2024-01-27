eff77773ad8cd0ac9b436a680eb715d98112b5ce
Mihail Mladenov <theoreticalphysicsftw@gmail.com>
2024-01-27 21:47:51 +0200

    Add another test image.

--- src/EmbeddedTestImage.hpp
+++ src/EmbeddedTestImage.hpp
@@ -3,8 +3,11 @@
 namespace PA
 {
 	static constexpr U32 CEmbeddedTestImageSize = 47690;
+	static constexpr U32 CEmbeddedMediumTestImageSize = 21478;
 	static constexpr U32 CEmbeddedSmallTestImageSize = 8854;
 
+
 	extern Byte GEmbeddedTestImageData[CEmbeddedTestImageSize];
+	extern Byte GEmbeddedMediumTestImageData[CEmbeddedMediumTestImageSize];
 	extern Byte GEmbeddedSmallTestImageData[CEmbeddedSmallTestImageSize];
 }
