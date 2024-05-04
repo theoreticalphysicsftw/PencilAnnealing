f961019bb6b6a3106749dd0e56908feb48969461
Mihail Mladenov <theoreticalphysicsftw@gmail.com>
2024-05-04 17:28:50 +0300

    Some cleanup.

--- src/Annealer.hpp
+++ src/Annealer.hpp
@@ -23,14 +23,11 @@
 
 #pragma once
 
-#include "Bezier.hpp"
-#include "Arc.hpp"
-#include "Line.hpp"
-#include "QuadTree.hpp"
+#include "Rendering.hpp"
 #include "Color.hpp"
 #include "Utilities.hpp"
 #include "Random.hpp"
-#include "File.hpp"
+#include "Serialization.hpp"
 #include "ThreadPool.hpp"
 #include "Convolution.hpp"
 
@@ -50,8 +47,6 @@ namespace PA
 		~Annealer();
 		auto CopyCurrentApproximationToColor(ColorU32* data, U32 stride) -> V;
 		auto AnnealBezier() -> B;
-		auto AnnealArc() -> V;
-		auto AnnealLine() -> B;
 
 		auto ClearSurface(RawCPUImage& img) -> V;
 		auto DrawLinesToSurface(const QuadTree<Line>& lines, RawCPUImage& img) -> Pair<U32, U32>;
@@ -60,23 +55,12 @@ namespace PA
 
 	private:
         static constexpr U32 logAfterSteps = 128;
-
-		auto ToScreenSpaceCoordinates(Vec in) -> Vec;
-		auto ToWorldCoordinates(Vec inScreen) -> Vec;
-		auto ToScreenSpaceCoordinates(Span<Vec> in) -> V;
-
-		auto DrawBezierToSurface(const QuadraticBezier& b, RawCPUImage& img) -> V;
-		auto DrawBezierToSurface(const Array<QuadraticBezier>& b, RawCPUImage& img) -> V;
-		auto DrawArcToSurface(const Arc& a, RawCPUImage& img) -> Pair<U32, U32>;
-		auto DrawArcsToSurface(const Array<Arc>& a, RawCPUImage& img) -> Pair<U32, U32>;
 		auto RestoreSurfaceFrom(RawCPUImage& toRestore, const RawCPUImage& reference, Pair<U32, U32> extent);
 
 		auto GetEnergy(const RawCPUImage& img0, Pair<U32, U32> extent) -> TF;
 		auto GetEnergy(const RawCPUImage& img0) -> TF;
 
 		auto InitBezier() -> V;
-		auto InitArc() -> V;
-		auto InitLine() -> V;
 
 		RawCPUImage grayscaleReference;
 		RawCPUImage grayscaleBlurredReference;
@@ -85,8 +69,6 @@ namespace PA
 		RawCPUImage workingApproximation;
 
 		Array<QuadraticBezier> strokes;
-		Array<Arc> arcStrokes;
-		QuadTree<Line> lineStrokes;
 
 		U32 maxStrokes;
 		U32 maxSteps;
@@ -129,7 +111,7 @@ namespace PA
 
 		grayscaleReferenceEdges = GradientMagnitude(threadPool, grayscaleReference);
 
-		this->maxStrokes = maxStrokes ? maxStrokes : (reference->width * reference->height / 256);
+		this->maxStrokes = maxStrokes ? maxStrokes : (reference->width * reference->height / 512);
 		this->maxSteps = maxSteps ? maxSteps : (1u << 24);
 		this->maxTemperature = 255 * 255;
 		temperature = maxTemperature;
@@ -137,30 +119,12 @@ namespace PA
 		optimalEnergy = GetEnergy(currentApproximation);
 
 		InitBezier();
-		//InitArc();
-		InitLine();
 	}
 
 	template<typename TF>
 	inline Annealer<TF>::~Annealer()
 	{
-		auto primitives = lineStrokes.GetSerializedPrimitives();
-		Str svg = "<svg xmlns = \"http://www.w3.org/2000/svg\" width =\"";
-		svg += ToString(currentApproximation.width);
-		svg += "\" height=\"" + ToString(currentApproximation.height) + "\"";
-		svg += " viewBox=\"0 0 " + ToString(currentApproximation.width) + " ";
-		svg += ToString(currentApproximation.height) + "\">\n";
-		svg += "<path stroke=\"#010101\" stroke-width=\"0.5\" d=\"";
-		for (auto& primitive : primitives)
-		{
-			auto p0 = ToScreenSpaceCoordinates(primitive.p0);
-			auto p1 = ToScreenSpaceCoordinates(primitive.p1);
-			svg += "M " + ToString(p0[0]) + " " + ToString(p0[1]) + " ";
-			svg += "L " + ToString(p1[0]) + " " + ToString(p1[1]) + " ";
-		}
-		svg += "\"/>\n";
-		svg += "</svg>";
-		WriteWholeFile("out.svg", { (const Byte*)svg.data(), svg.size() });
+		SerializeToSVG(Span<const QuadraticBezier>(strokes), grayscaleReference.width, grayscaleReference.height);
 	}
 
 	template<typename TF>
@@ -172,40 +136,6 @@ namespace PA
 			strokes.push_back(GetRandom2DQuadraticBezierInRange(strokeLength));
 		}
 	}
-
-	template<typename TF>
-	inline auto Annealer<TF>::InitArc() -> V
-	{
-		for (auto i = 0u; i < maxStrokes; ++i)
-		{
-			auto strokeLength = SmoothStep(TF(1) / grayscaleReference.width, TF(0.5), TF(0.5) * (1 - TF(i + 1) / maxStrokes));
-			auto arcStart = GetUniformFloat<TF>() * Constants<TF>::C2Pi;
-			auto arcEnd = GetUniformFloat<TF>() * Constants<TF>::C2Pi;
-			auto center = Vec2(GetUniformFloat<TF>(), GetUniformFloat<TF>());
-			auto radius = GetUniformFloat<TF>() * strokeLength;
-			arcStrokes.emplace_back(center, radius, arcStart, arcEnd);
-		}
-	}
-
-
-	template<typename TF>
-	inline auto Annealer<TF>::InitLine() -> V
-	{
-		Array<Line> randomLines;
-		for (auto i = 0u; i < maxStrokes; ++i)
-		{
-			auto strokeLength = SmoothStep(TF(4) / grayscaleReference.width, TF(0.2), TF(0.2) * (1 - TF(i + 1) / maxStrokes));
-			auto p0 = Vec2(GetUniformFloat<TF>(), GetUniformFloat<TF>());
-			auto angle = GetUniformFloat<TF>() * Constants<TF>::C2Pi;
-			auto direction = Vec2(Cos(angle), Sin(angle)) * strokeLength;
-			PA_ASSERT(direction.Length() <= 1);
-			auto p1 = p0 + direction;
-			randomLines.emplace_back(p0, p1);
-		}
-		lineStrokes.Build({randomLines.begin(), randomLines.end()});
-		Log("QuadTree built.");
-	}
-
 
 	template<typename TF>
 	inline auto Annealer<TF>::ClearSurface(RawCPUImage& img) -> V
@@ -263,7 +193,7 @@ namespace PA
 			return false;
 		}
 
-		temperature = temperature * TF(0.999);
+		temperature = temperature * TF(0.99);
 
 		auto strokeIdx = GetUniformU32(0, strokes.size() - 1);
 
@@ -272,7 +202,7 @@ namespace PA
 		*targetCurvePtr = GetRandom2DQuadraticBezierInRange(TF(1));
 
 		ClearSurface(workingApproximation);
-		DrawBezierToSurface(strokes, workingApproximation);
+		RasterizeToGSSurfaceUnsafe(Span<const QuadraticBezier>(strokes), workingApproximation, threadPool);
 		auto currentEnergy = GetEnergy(workingApproximation);
 
 		auto transitionThreshold = Exp((optimalEnergy - currentEnergy) / temperature);
@@ -299,313 +229,6 @@ namespace PA
 	}
 
 
-	template<typename TF>
-	inline auto Annealer<TF>::AnnealArc() -> V
-	{
-		if (step >= maxSteps)
-		{
-			return;
-		}
-
-		temperature = TF(1) - TF(step) / maxSteps;
-
-		
-		auto strokeIdx = GetUniformU32(0, arcStrokes.size() - 1);
-		auto propertyIdx = GetUniformU32(0, 4);
-		auto preturbation = GetUniformFloat<TF>();
-
-		auto propertyPtr = (TF*) &arcStrokes[strokeIdx];
-		propertyPtr[propertyIdx] = preturbation;
-
-		ClearSurface(workingApproximation);
-		DrawArcsToSurface(arcStrokes, workingApproximation);
-		auto currentEnergy = GetEnergy(workingApproximation);
-
-		auto transitionThreshold = Exp((optimalEnergy - currentEnergy) / temperature);
-
-		if (currentEnergy < optimalEnergy || transitionThreshold >= GetUniformFloat<TF>())
-		{
-			optimalEnergy = currentEnergy;
-			currentApproximation = workingApproximation;
-		}
-		else
-		{
-			propertyPtr[propertyIdx] -= preturbation;
-		}
-	}
-
-	template<typename TF>
-	inline auto Annealer<TF>::AnnealLine() -> B
-	{
-		if (step >= maxSteps)
-		{
-			Log("Annealing done.");
-			return false;
-		}
-
-
-		temperature = temperature * TF(0.999);
-
-		auto line = lineStrokes.GetRandomPrimitive();
-		auto oldLine = line;
-		auto oldLength = Distance(oldLine.p0, oldLine.p1);
-		B appropriatePreturbation = false;
-		BBox field(Vec(0, 0), Vec(1, 1));
-		while (!appropriatePreturbation)
-		{
-			auto newAngle = GetUniformFloat<TF>() * Constants<TF>::C2Pi;
-			auto newDirection = Vec(Sin(newAngle), Cos(newAngle)) * oldLength;
-			auto newStart = Vec(GetUniformFloat<TF>(), GetUniformFloat<TF>());
-			line.p0 = newStart;
-			line.p1 = newStart + newDirection;
-
-			if (field.Contains(line))
-			{
-				appropriatePreturbation = true;
-			}
-			else
-			{
-				line = oldLine;
-			}
-		}
-
-
-		lineStrokes.Remove(oldLine);
-		lineStrokes.Add(line);
-		ClearSurface(workingApproximation);
-		DrawLinesToSurface(lineStrokes, workingApproximation);
-		auto currentEnergy = GetEnergy(workingApproximation);
-
-		auto transitionThreshold = Exp((optimalEnergy - currentEnergy) / temperature);
-
-		if (currentEnergy < optimalEnergy || transitionThreshold >= GetUniformFloat<TF>())
-		{
-			optimalEnergy = currentEnergy;
-			if (!(step % logAfterSteps) || step == maxSteps - 1)
-			{
-				currentApproximation = workingApproximation;
-			}
-		}
-		else
-		{
-			lineStrokes.Remove(line);
-			lineStrokes.Add(oldLine);
-		}
-		step++;
-
-
-		if (!(step % logAfterSteps))
-		{
-			Log("Energy = ", optimalEnergy, " Temperature = ", temperature);
-		}
-
-		return true;
-	}
-
-
-	template<typename TF>
-	inline auto Annealer<TF>::ToScreenSpaceCoordinates(Vec in) -> Vec
-	{
-		Vec result;
-		result[0] = in[0] * (grayscaleReference.width - 1);
-		result[1] = (TF(1) - in[1]) * (grayscaleReference.height - 1);
-		return result;
-	}
-
-	template<typename TF>
-	inline auto Annealer<TF>::ToWorldCoordinates(Vec inScreen) -> Vec
-	{
-		Vec result;
-		result[0] = inScreen[0] / (grayscaleReference.width - 1);
-		result[1] = TF(1) - inScreen[1] / (grayscaleReference.height - 1);
-		return result;
-	}
-
-
-	template<typename TF>
-	inline auto Annealer<TF>::ToScreenSpaceCoordinates(Span<Vec> in) -> V
-	{
-		for (auto& vec : in)
-		{
-			vec = ToScreenSpaceCoordinates(vec);
-		}
-	}
-
-
-	template<typename TF>
-	inline auto Annealer<TF>::DrawBezierToSurface(const QuadraticBezier& curve, RawCPUImage& img) -> V
-	{
-		auto bBox = curve.GetBBox();
-		auto lowerLeft = ToScreenSpaceCoordinates(bBox.lower);
-		auto upperRight = ToScreenSpaceCoordinates(bBox.upper);
-		auto lowerRight = Vec(upperRight[0], lowerLeft[1]);
-		auto upperLeft = Vec(lowerLeft[0], upperRight[1]);
-		auto imgSize = img.width * img.height;
-
-		auto screenCurve = curve;
-		ToScreenSpaceCoordinates(screenCurve.points);
-
-        Array<QuadraticBezier> stack;
-        stack.push_back(screenCurve);
-
-        while (!stack.empty())
-        {
-            auto current = stack.back();
-            stack.pop_back();
-
-            auto roughApproxLength = (current[0] - current[1]).Length() + (current[2] - current[1]).Length();
-
-            if (roughApproxLength <= Scalar(1))
-            {
-                auto centroid = current.GetCentroid();
-                auto x = U16(centroid[0]);
-                auto y = U16(centroid[1]);
-                auto i = LebesgueCurve(x, y);
-                if (i >= img.data.size())
-                {
-                    continue;
-                }
-
-                auto pixelCenter = Vec(x, y) + Scalar(0.5);
-                auto dist = current.GetDistanceFrom(pixelCenter);
-                auto val = SmoothStep(TF(0), TF(1), dist);
-                img.data[i] = ClampedU8(img.data[i] - (TF(255) - TF(255) * val));
-            }
-            else
-            {
-                auto split = current.Split(Scalar(0.5));
-                stack.push_back(split.first);
-                stack.push_back(split.second);
-            }
-        }
-	}
-
-
-	template<typename TF>
-	inline auto Annealer<TF>::DrawBezierToSurface(const Array<QuadraticBezier>& curves, RawCPUImage& img) -> V
-	{
-
-		auto taskCount = GetLogicalCPUCount();
-		auto curvesPerTask = curves.size() / taskCount;
-
-		auto task =
-		[&](U32 start, U32 end)
-		{
-			for (auto i = start; i < end; ++i)
-			{
-                DrawBezierToSurface(curves[i], img);
-			}
-		};
-
-		Array<TaskResult<V>> results;
-		for (auto i = 0u; i < taskCount; ++i)
-		{
-			results.emplace_back(threadPool.AddTask(task, i * curvesPerTask, (i + 1) * curvesPerTask));
-		}
-
-		// Remainder
-		task(taskCount * curvesPerTask, curves.size());
-
-		for (auto& result : results)
-		{
-			result.Retrieve();
-		}
-	}
-
-
-	template<typename TF>
-	inline auto Annealer<TF>::DrawArcToSurface(const Arc& a, RawCPUImage& img) -> Pair<U32, U32>
-	{
-		auto bBox = a.GetBBox();
-		auto lowerLeft = ToScreenSpaceCoordinates(bBox.lower);
-		auto upperRight = ToScreenSpaceCoordinates(bBox.upper);
-		auto lowerRight = Vec(upperRight[0], lowerLeft[1]);
-		auto upperLeft = Vec(lowerLeft[0], upperRight[1]);
-		auto imgSize = img.width * img.height;
-		auto minIdx = Min(LebesgueCurve(upperLeft[0], upperLeft[1]), imgSize - 1);
-		auto maxIdx = Min(LebesgueCurve(lowerRight[0], lowerRight[1]), imgSize - 1);
-
-		auto screenArc = a;
-		screenArc.center = ToScreenSpaceCoordinates(screenArc.center);
-		screenArc.radius *= img.width;
-
-		for (auto i = minIdx; i <= maxIdx; ++i)
-		{
-			auto coords = LebesgueCurveInverse(i);
-			auto dist = screenArc.GetDistanceFrom(Vec(coords.first, coords.second));
-			auto val = SmoothStep(TF(0), TF(1), dist / 1.f);
-			img.data[i] = ClampedU8(img.data[i] - (TF(255) - TF(255) * val));
-		}
-
-		return Pair<U32, U32>(minIdx, maxIdx);
-	}
-
-
-	template<typename TF>
-	inline auto Annealer<TF>::DrawArcsToSurface(const Array<Arc>& arcs, RawCPUImage& img) -> Pair<U32, U32>
-	{
-		auto maxExtent = Pair<U32, U32>(0, 0);
-		for (auto& arc : arcs)
-		{
-			auto extent = DrawArcToSurface(arc, img);
-			maxExtent.first = Max(maxExtent.first, extent.first);
-			maxExtent.second = Max(maxExtent.second, extent.second);
-		}
-		return maxExtent;
-	}
-	
-
-	template<typename TF>
-	inline auto PA::Annealer<TF>::DrawLinesToSurface(const QuadTree<Line>& lines, RawCPUImage& img) -> Pair<U32, U32>
-	{
-		auto imgSize = img.width * img.height;
-
-		auto taskCount = GetLogicalCPUCount();
-		auto pixelsPerTask = imgSize / taskCount;
-
-		auto task =
-		[&](U32 start, U32 end)
-		{
-			for (auto i = start; i < end; ++i)
-			{
-				auto coords = LebesgueCurveInverse(i);
-				auto floatCoords = Vec(coords.first + 0.5, coords.second + 0.5);
-				auto worldCoords = ToWorldCoordinates(floatCoords);
-				auto nearLines = lines.GetPrimitivesAround(worldCoords);
-
-				for (auto& l : nearLines)
-				{
-					auto screenLine = l;
-					ToScreenSpaceCoordinates(screenLine.points);
-
-					auto dist = screenLine.GetDistanceFrom(floatCoords);
-					auto val = SmoothStep(TF(0), TF(1), dist);
-					img.data[i] = ClampedU8(img.data[i] - (TF(255) - TF(255) * val));
-
-					if (img.data[i] == 0)
-					{
-						break;
-					}
-				}
-			}
-		};
-
-		Array<TaskResult<V>> results;
-		for (auto i = 0u; i < taskCount; ++i)
-		{
-			results.emplace_back(threadPool.AddTask(task, i * pixelsPerTask, (i + 1) * pixelsPerTask));
-		}
-
-		// Remainder
-		task(taskCount * pixelsPerTask, imgSize);
-
-		for (auto& result : results)
-		{
-			result.Retrieve();
-		}
-
-		return Pair<U32, U32>(0, imgSize);
-	}
 
 	template<typename TF>
 	inline auto Annealer<TF>::ShutDownThreadPool() -> V
