7b09696313d7d51a846a6f32e35d386982814d74
Mihail Mladenov <theoreticalphysicsftw@gmail.com>
2024-05-07 20:37:26 +0300

    Enable webp encoding.

--- src/Webp.hpp
+++ src/Webp.hpp
@@ -24,30 +24,52 @@
 #pragma once
 
 #include <libwebp/src/webp/decode.h>
+#include <libwebp/src/webp/encode.h>
 
 #include "Types.hpp"
 #include "Image.hpp"
+#include "Error.hpp"
 
 namespace PA
 {
-	inline auto DecodeWebP(Byte* data, U32 size) -> RawCPUImage;
+	inline auto DecodeWebP(Span<const Byte> data) -> RawCPUImage;
+	inline auto EncodeWebP(const RawCPUImage& img, F32 qf = 0.5f) -> Array<Byte>;
 }
 
 namespace PA
 {
-	inline auto DecodeWebP(Byte* data, U32 size) -> RawCPUImage
+	inline auto DecodeWebP(Span<const Byte> data) -> RawCPUImage
 	{
 		RawCPUImage result;
 		result.format = EFormat::RGBA8;
 		I32 width, height;
 
-		WebPGetInfo(data, size, &width, &height);
+		WebPGetInfo(data.data(), data.size(), &width, &height);
 		result.width = width;
 		result.height = height;
 		result.data.resize(result.height * result.width * sizeof(U32));
 
-		WebPDecodeRGBAInto(data, size, result.data.data(), result.data.size(), result.width * sizeof(U32));
+		auto decodeResult =
+			WebPDecodeRGBAInto(data.data(), data.size(), result.data.data(), result.data.size(), result.width * sizeof(U32));
 
+		if (decodeResult == nullptr)
+		{
+			result.data.clear();
+			result.width = 0;
+			result.height = 0;
+		}
+
+		return result;
+	}
+
+	inline auto EncodeWebP(const RawCPUImage& img, F32 qf) -> Array<Byte>
+	{
+		PA_ASSERT(img.format == EFormat::RGBA8);
+		U8* out;
+		auto size = WebPEncodeRGBA(img.data.data(), img.width, img.height, img.width * 4, qf, &out);
+		// Unnecessary copy but whatever it's not performance critical function.
+		Array<Byte> result(out, out + size);
+		WebPFree(out);
 		return result;
 	}
 }
