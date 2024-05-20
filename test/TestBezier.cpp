a99edc468b34c2c2b86e163d00fdebe9dcbf90ed
Mihail Mladenov <theoreticalphysicsftw@gmail.com>
2024-05-20 21:36:21 +0300

    Switch to GPL3.0 .

--- test/TestBezier.cpp
+++ test/TestBezier.cpp
@@ -1,3 +1,21 @@
+// Copyright 2024 Mihail Mladenov
+//
+// This file is part of PencilAnnealing.
+//
+// PencilAnnealing is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PencilAnnealing is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PencilAnnealing.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #include <Error.hpp>
 #include <Bezier.hpp>
 
@@ -6,7 +24,7 @@ using namespace PA;
 auto Test0() -> V
 {
 	static constexpr U32 latticeSize = 10000;
-	static constexpr F32 tolerance = 0.0001;
+	static constexpr F32 tolerance = 0.0001f;
 
 	QuadraticBezier<F32, 2> curve(Vec2(-1, -1), Vec2(0, 1), Vec2(1, -1));
 	for (auto i = 0; i < latticeSize; ++i)
--- test/TestUtilities.cpp
+++ test/TestUtilities.cpp
@@ -1,3 +1,21 @@
+// Copyright 2024 Mihail Mladenov
+//
+// This file is part of PencilAnnealing.
+//
+// PencilAnnealing is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PencilAnnealing is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PencilAnnealing.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #include <Types.hpp>
 #include <Error.hpp>
 #include <Utilities.hpp>
@@ -20,7 +38,7 @@ I32 main(I32 argc, const C** argv)
 	}
 
 	static constexpr U32 latticeSize = 300;
-	static constexpr F32 tolerance = 0.01;
+	static constexpr F32 tolerance = 0.01f;
 	U32 rootsFound = 0;
 	for (auto i = 0; i < latticeSize; ++i)
 	{
